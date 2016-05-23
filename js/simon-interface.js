9c0b097036674581b1c5a73dd642ec10073722ca
Jeffrey Hackford and Kevin Deganos <jeffrey@>
2016-05-23 16:22:45 -0700

    add flash animation between colors

--- js/simon-interface.js
+++ js/simon-interface.js
@@ -9,26 +9,33 @@ $(document).ready(function() {
   var simon = new Simon();
 
   var userColors = [];
-  var simonColors = [];
+  var simonColors = [simon.randomColor()];
 
   blue.onclick = function() {
     userColors.push(blue.getAttribute("value"));
+    console.log(userColors);
   };
 
   red.onclick = function() {
     userColors.push(red.getAttribute("value"));
+    console.log(userColors);
   };
 
   yellow.onclick = function() {
     userColors.push(yellow.getAttribute("value"));
+    console.log(userColors);
   };
 
   green.onclick = function() {
     userColors.push(green.getAttribute("value"));
+    console.log(userColors);
   };
 
-  $('#submit').click(function(event){
-    event.preventDefault();
+  $('#ready').click(function() {
+    simon.pattern(simonColors);
+  });
+
+  $('#submit').click(function(){
     if(simon.compare(userColors, simonColors)) {
       $('#correct').append('<p>' + "You are correct!" + "</p>");
       simonColors.push(simon.randomColor());
@@ -36,7 +43,7 @@ $(document).ready(function() {
       console.log(simonColors);
     } else {
       $('#correct').append('<p>' + "You not correct!" + "</p>");
-      simonColors = [];
+      simonColors = [simon.randomColor()];
       userColors = [];
       console.log("second");
 
--- js/simon.js
+++ js/simon.js
@@ -3,7 +3,7 @@ exports.Simon = function() {
 };
 
 exports.Simon.prototype.randomColor = function() {
-  var colors = [blue, red, yellow, green];
+  var colors = ["blue", "red", "yellow", "green"];
   var rand = colors[Math.floor(Math.random() * colors.length)];
   return rand;
 };
@@ -12,13 +12,44 @@ exports.Simon.prototype.compare = function(userColors, simonColors) {
   if(userColors.length != simonColors.length) {
     return false;
   } else {
-    for(i = 0; i < userColors.length; i++){
-      for(j = 0; j < simonColors.length; j++){
-        if(userColors[i] != simonColors[j]){
+    for(i = 0; i < simonColors.length; i++){
+        if(userColors[i] != simonColors[i]){
           return false;
         }
-      }
     }
       return true;
+  }
+};
+
+exports.Simon.prototype.pattern = function(simonColors) {
+  var i = 0;
+  var interval = setInterval(doStuff, 1500);
+    function doStuff() {
+        console.log(simonColors[i]);
+        if(simonColors[i] == "blue") {
+        $('#blue').animateCss("flash");
+        i++;
+      } else if (simonColors[i] == "red"){
+        $('#red').animateCss("flash");
+        i++;
+      } else if (simonColors[i] == "yellow"){
+        $('#yellow').animateCss("flash");
+        i++;
+      } else if (simonColors[i] == "green"){
+        $('#green').animateCss("flash");
+        i++;
+      }
+      if(i === simonColors.length) {
+        clearInterval(interval);
+      }
     }
-  };
+  }
+
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
